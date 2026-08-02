47d70664fe943655245e5e374c5aee04fd4e4e3b
june <0721hjp@naver.com>
2026-08-02 17:20:20 +0900

    feat: BaseEntity 추가

--- src/test/java/com/daejin/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/daejin/capstone/CapstoneApplicationTests.java
@@ -2,8 +2,11 @@ package com.daejin.capstone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootTest
+@EnableJpaAuditing
 class CapstoneApplicationTests {
 
 	@Test
