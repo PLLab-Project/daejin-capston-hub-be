17bca147624275528478a14b1494799e435bb4c9
june <0721hjp@naver.com>
2026-08-02 14:29:22 +0900

    chore: 스웨거 엔드포인트 허용

--- src/main/java/com/daejin/capstone/global/security/SecurityConfig.java
+++ src/main/java/com/daejin/capstone/global/security/SecurityConfig.java
@@ -1,5 +1,70 @@
 package com.daejin.capstone.global.security;
 
+import com.daejin.capstone.global.common.response.ResponseDTO;
+import com.daejin.capstone.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http, ObjectMapper objectMapper) throws Exception {
+
+    // 인증 실패 시 응답 객체
+    String invalidAuthenticationResponse = objectMapper
+        .writeValueAsString(ResponseDTO.of(ErrorCode.UNAUTHORIZED));
+
+    // 인가 실패 시 응답 객체
+    String invalidAuthorizationResponse = objectMapper
+        .writeValueAsString(ResponseDTO.of(ErrorCode.ACCESS_DENIED));
+
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/users/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/v3/api-docs/**",
+                "/oauth2/**",
+                "/login/**"
+            ).permitAll()
+            .anyRequest().authenticated()
+        );
+
+    http
+        .exceptionHandling(e -> e
+            .authenticationEntryPoint(((request, response, authException) -> {
+              response.setContentType("application/json;charset=UTF-8");
+              response.setStatus(HttpStatus.UNAUTHORIZED.value());
+              response.setContentType("application/json");
+              response.getWriter().write(invalidAuthenticationResponse);
+            }
+            ))
+
+            .accessDeniedHandler((request, response, authException) -> {
+              response.setContentType("application/json;charset=UTF-8");
+              response.setStatus(HttpStatus.FORBIDDEN.value());
+              response.setContentType("application/json");
+              response.getWriter().write(invalidAuthorizationResponse);
+            }));
+
+
+    return http.build();
+
+
+  }
+
 
 }
